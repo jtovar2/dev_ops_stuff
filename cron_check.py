183a30484a4f58264f97628de74bf21c39e824d1
jtovar2 <jtovar2@github.com>
2018-09-04 23:02:12 -0400

    ayyyeee progress

--- cron_check.py
+++ cron_check.py
@@ -2,14 +2,15 @@ import os
 import time
 import subprocess
 from google.cloud import datastore
-
+import requests
+import json
 
 LCP = os.environ['LCP']
-DOGECOIN_EXECUTABLES_LOCATION = 'nohup /home/javier/dogecoin-1.10.0/bin/dogecoind &'
+DOGECOIN_EXECUTABLES_LOCATION = '/home/javier/dogecoin-1.10.0/bin/dogecoind'
 DIGIBYTE_EXECUTABLES_LOCATION = '/home/javier/digibyte-6.16.2/bin/digibyted'
 MAX_RETRY = 5
 
-store_client = datastore.Client()
+datastore_client = datastore.Client()
 
 daemon_to_exec_map = {
         'dogecoin': DOGECOIN_EXECUTABLES_LOCATION,
@@ -17,16 +18,13 @@ daemon_to_exec_map = {
         }
 
 def start_daemon(daemon_cmd):
-    response = os.system(daemon_cmd)
-
-
-    if response == 0:
-        return True
-    else:
-        return False
+    print("about to start this daemon")
+    subprocess.Popen(['nohup', daemon_cmd],stdout=open('/dev/null', 'w'),
+            stderr=open('/home/javier/logfile.log', 'a'),
+            preexec_fn=os.setpgrp)
+    print("daemon started")
 
 def check_that_service_is_running(daemon):
-    print(daemon)
     output = subprocess.check_output(['ps', '-A'])
     if daemon not in output:
         start_daemon( daemon_to_exec_map[daemon] )
@@ -39,9 +37,14 @@ def check_ping():
         pingstatus = True
     else:
         pingstatus = False
+        print("ping failed so sleep")
+        time.sleep(30)
     return pingstatus
 
 def update_ip():
+    ip_api_address = 'https://api.ipify.org/?format=json'
+    response = requests.get(ip_api_address)
+    json_dict = response.json()
     # The kind for the new entity
     kind = 'wallet_{LCP}'.format(LCP=LCP)
     # The name/ID for the new entity
@@ -51,7 +54,7 @@ def update_ip():
 
     # Prepares the new entity
     task = datastore.Entity(key=task_key)
-    task['IP'] = 'test'
+    task['IP'] = json_dict['ip']
 
     # Saves the entity
     datastore_client.put(task)
@@ -59,12 +62,11 @@ def update_ip():
     print('Saved {}: {}'.format(task.key.name, task['IP']))
 
 internet_conn = check_ping()
-
 if not internet_conn:
     response = os.system("dhclient")
     internet_conn = check_ping()
     retry_count = MAX_RETRY
-    while not internet_conn and retry_count > 0:
+    while not internet_conn or retry_count > 0:
         response = os.system("dhclient")
         time.sleep(30)
         internet_conn = check_ping()
@@ -74,19 +76,13 @@ internet_conn = check_ping()
 if not internet_conn:
     exit()
 
+else:
+    print("there is internet connection")
 
 
 for daemon in daemon_to_exec_map:
     if check_that_service_is_running(daemon):
-        result = start_deamon(daemon)
-        retry_count = MAX_RETRY
-        while not result and retry_count > 0:
-            result = start_daemon(daemon)
-            retry_count = retry_count - 1
-
-        if not result:
-            ##unable to start daemon
-            print("prob with {deamon}".format(daemon=daemon))
+        spawn_deamon(start_daemon,daemon)
 
 update_ip()
 
